f9389fe727636ffa61cce8a52e0b12b890344fa0
shencang <2630610009@qq.com>
2020-04-03 14:00:30 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -116,4 +116,66 @@ const router = new VueRouter({
     routes
 });
 
+// eslint-disable-next-line no-unused-vars
+const createRouter = new VueRouter({
+    mode:'history',
+    routerD
+});
+const routerD =[{
+    path: '/',
+    name: 'Default',
+    redirect: '/home',
+    component: Home
+},
+    {
+        // home页面并不需要被访问，只是作为其它组件的父组件
+        path: '/home',
+        name: 'Home',
+        component: Home,
+        redirect: '/index',
+        children: [
+            {
+                path: '/index',
+                name: 'AppIndex',
+                component: AppIndex
+            },
+            {
+                path: '/admin/content/editor',
+                name: 'Editor',
+                component: Editor,
+                meta: {
+                    requireAuth: true
+                }
+            },
+        ]
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    },
+    {
+        path: '/register',
+        name: 'Register',
+        component: Register
+    },
+    {
+        path: '/admin',
+        name: 'Admin',
+        component: AdminIndex,
+        meta: {
+            requireAuth: true
+        },
+        children: [
+            {
+                path: '/admin/dashboard',
+                name: 'dashboard',
+                component: DashBoard,
+                meta: {
+                    requireAuth: true
+                }
+            }
+        ]
+    }];
+
 export default router
