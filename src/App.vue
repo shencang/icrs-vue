1572322a711c81c84c067f67f1997ad54b5cff36
shencang <2630610009@qq.com>
2020-06-10 23:43:30 +0800

    fix text 修复文本

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <template>
   <div id="app">
-<!--    <div id="nav">-->
-<!--      <router-link to="/">主页</router-link> |-->
-<!--      <router-link to="/about">关于</router-link> |-->
-<!--      <router-link to="/login">登录</router-link>-->
-<!--    </div>-->
     <router-view/>
   </div>
 </template>
@@ -25,16 +20,4 @@
   color: #2c3e50;
 }
 
-/*#nav {*/
-/*  padding: 1px;*/
-/*}*/
-
-/*#nav a {*/
-/*  font-weight: bold;*/
-/*  color: #2c3e50;*/
-/*}*/
-
-/*#nav a.router-link-exact-active {*/
-/*  color: #42b983;*/
-/*}*/
 </style>
